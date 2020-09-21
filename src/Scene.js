96ee1c69b29bae998a4a7fef36f7f77839ceabfa
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-09-21 17:39:03 +0200

    better material reuse

--- src/Scene.js
+++ src/Scene.js
@@ -1,30 +1,35 @@
 import React, { useMemo, useRef, useState } from 'react'
 import * as THREE from 'three'
-import { useFrame, useThree } from 'react-three-fiber'
-import { Text, Box, useMatcapTexture, Octahedron } from 'drei'
+import { useFrame, useThree, useResource } from 'react-three-fiber'
+import { Text, Box, useMatcapTexture, Octahedron, OrbitControls } from 'drei'
 
 import { ThinFilmFresnelMap } from './ThinFilmFresnelMap'
 import { mirrorsData, textData } from './data'
 
-const FONT = 'https://fonts.gstatic.com/s/raleway/v17/1Ptxg8zYS_SKggPN4iEgvnHyvveLxVtzpbCIPrcVIT9d0c8.woff'
+const textProps = {
+  fontSize: 3.9,
+  font: 'https://fonts.gstatic.com/s/raleway/v17/1Ptxg8zYS_SKggPN4iEgvnHyvveLxVtzpbCIPrcVIT9d0c8.woff'
+}
+
 
 function ResponsiveText(props) {
+  
   return (
     <group {...props}>
-      <Text position={[-1.8, 0.4, 0]} fontSize={3.8} font={FONT}>
+      <Text position={[-1.8, 0.4, 0]} {...textProps}>
         R
       </Text>
-      <Text position={[0, -0.6, 0]} rotation={[0, 0, -Math.PI / 16]} fontSize={3.8} font={FONT}>
+      <Text position={[0, -0.6, 0]} rotation={[0, 0, -Math.PI / 16]} {...textProps}>
         3
       </Text>
-      <Text position={[1.5, 0.2, 0]} scale={[-1, 1, 1]} fontSize={3.8} font={FONT}>
+      <Text position={[1.5, 0.2, 0]} scale={[-1, 1, 1]} {...textProps}>
         F
       </Text>
     </group>
   )
 }
 
-function Mirror({ material, texture, args, map, ...props }) {
+function Mirror({ sideMaterial, reflectionMaterial, args, ...props }) {
   const ref = useRef()
 
   useFrame(() => {
@@ -33,29 +38,52 @@ function Mirror({ material, texture, args, map, ...props }) {
   })
 
   return (
-    <group {...props}>
-      <Box ref={ref} args={args}>
-        <meshLambertMaterial attachArray="material" map={map} color={0xaaaaaa} />
-        <meshLambertMaterial attachArray="material" map={map} color={0xaaaaaa} />
-        <meshLambertMaterial attachArray="material" map={map} color={0xaaaaaa} />
-        <meshLambertMaterial attachArray="material" map={map} color={0xaaaaaa} />
-        <meshLambertMaterial attachArray="material" envMap={texture} map={map} />
-        <meshLambertMaterial attachArray="material" envMap={texture} map={map} />
-      </Box>
-    </group>
+    <Box {...props} ref={ref} args={args}
+      material={[
+        sideMaterial,
+        sideMaterial,
+        sideMaterial,
+        sideMaterial,
+        reflectionMaterial,
+        reflectionMaterial
+      ]}
+    />
   )
 }
 
+function Mirrors({ envMap }) {
+
+  const sideMaterial = useResource()
+  const reflectionMaterial = useResource()
+  const [thinFilmFresnelMap] = useState(new ThinFilmFresnelMap())
+  
+  return (
+    <group name="mirrors">
+
+        <meshLambertMaterial ref={sideMaterial} map={thinFilmFresnelMap} color={0xaaaaaa} />
+        <meshLambertMaterial ref={reflectionMaterial} map={thinFilmFresnelMap} envMap={envMap}/>
+      
+        {mirrorsData.mirrors.map((mirror, index) => (
+          <Mirror 
+            key={`0${index}`} {...mirror} 
+            sideMaterial={sideMaterial.current}
+            reflectionMaterial={reflectionMaterial.current}
+          />
+        ))}
+      </group>
+  )
+
+}
+
 export default function Scene() {
-  const [cubeRenderTarget] = useState(
+  const [renderTarget] = useState(
     new THREE.WebGLCubeRenderTarget(1024, { format: THREE.RGBAFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter })
   )
-  const [thinFilmFresnelMap] = useState(new ThinFilmFresnelMap())
 
   const camera = useRef()
   const sphere = useRef()
 
-  const [matcapTexture] = useMatcapTexture('C8D1DC_575B62_818892_6E747B')
+  const [matcapTexture] = useMatcapTexture('C8D1DC_575B62_818892_6E747B', 1024)
 
   useFrame(({ gl, scene }) => {
     sphere.current.visible = true
@@ -67,39 +95,36 @@ export default function Scene() {
 
   const { viewport } = useThree()
 
-  const [posV, rotE, rotQ] = useMemo(() => {
-    return [new THREE.Vector3(0, 0, 0), new THREE.Euler(0, 0, 0), new THREE.Quaternion(0, 0, 0, 0)]
+  const [rotationEuler, rotationQuaternion] = useMemo(() => {
+    return [
+      new THREE.Euler(0, 0, 0), 
+      new THREE.Quaternion(0, 0, 0, 0)
+    ]
   }, [])
 
   useFrame(({ mouse }) => {
     const x = (mouse.x * viewport.width) / 100
     const y = (mouse.y * viewport.height) / 100
 
-    posV.set(x, y, 0)
-    rotE.set(y, x, 0)
+    rotationEuler.set(y, x, 0)
+    rotationQuaternion.setFromEuler(rotationEuler)
 
-    rotQ.setFromEuler(rotE)
-
-    group.current.position.lerp(posV, 0.1)
-    group.current.quaternion.slerp(rotQ, 0.1)
+    group.current.quaternion.slerp(rotationQuaternion, 0.1)
   })
 
   return (
     <group ref={group}>
-      <cubeCamera ref={camera} args={[0.1, 100, cubeRenderTarget]} position={[0, 0, 5]} />
-      <group name="mirrors">
-        {mirrorsData.mirrors.map((mirror, index) => (
-          <Mirror key={`0${index}`} {...mirror} texture={cubeRenderTarget.texture} map={thinFilmFresnelMap} />
-        ))}
-      </group>
-      <group name="text" position={[0, 0, 5]}>
-        {textData.map((data, index) => (
-          <ResponsiveText key={`0${index}`} {...data} />
-        ))}
-      </group>
       <Octahedron ref={sphere} args={[20, 4, 4]} position={[0, 0, -5]}>
         <meshMatcapMaterial matcap={matcapTexture} side={THREE.BackSide} transparent opacity={0.3} />
       </Octahedron>
+      <cubeCamera ref={camera} args={[0.1, 100, renderTarget]} position={[0, 0, 5]} />
+      
+      <Mirrors envMap={renderTarget.texture} />
+      
+      <group name="text" position={[0, 0, 5]}>
+        {textData.map((data, index) => ( <ResponsiveText key={index} {...data} /> ))}
+      </group>
+      <OrbitControls />
     </group>
   )
 }
